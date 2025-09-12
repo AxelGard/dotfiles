5ef3bc343f29ebd3187e8fef2cbf7d2bd5dfa76f
Axel Gard <axel.gard@protonmail.com>
2025-09-12 11:48:52 +0200

    added hammer spoon to spilt in two and move to left and right

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -18,4 +18,31 @@ hs.hotkey.bind({"cmd","alt"}, "0", function()
 end)
 
 
+-- Move current window to left half of screen
+hs.hotkey.bind({"cmd"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-- Move current window to right half of screen
+hs.hotkey.bind({"cmd"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
 
