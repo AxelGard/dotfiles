e17d7399bd5ceca399ba19ae05cf4acd2f08da76
Axel Gard <axel.gard@protonmail.com>
2025-09-12 11:42:13 +0200

    made hs better keys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,14 @@
-
 hs = hs 
 
-hs.hotkey.bind({"cmd","alt"}, "1", function()
-    hs.application.launchOrFocusByBundleID("com.googlecode.iterm2")
-end)
-
-hs.hotkey.bind({"cmd","alt"}, "2", function()
+hs.hotkey.bind({"cmd"}, "1", function()
     hs.application.launchOrFocus("Brave Browser")
 end)
 
-hs.hotkey.bind({"cmd","alt"}, "3", function()
+hs.hotkey.bind({"cmd"}, "2", function()
+    hs.application.launchOrFocusByBundleID("com.googlecode.iterm2")
+end)
+
+hs.hotkey.bind({"cmd"}, "3", function()
     hs.application.launchOrFocusByBundleID("com.microsoft.VSCode")
 
 end)
