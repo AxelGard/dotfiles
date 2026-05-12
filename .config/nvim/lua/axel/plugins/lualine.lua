c99eac534dae6b032cab6f6f234633885c295a35
Axel Gard <axel.gard@protonmail.com>
2026-05-12 16:37:40 +0200

    nvim added lsp status in lualine

--- .config/nvim/lua/axel/plugins/lualine.lua
+++ .config/nvim/lua/axel/plugins/lualine.lua
@@ -10,6 +10,20 @@ end
 local lualine_theme = require("lualine.themes.oneokai")
 
 
+-- show names of LSP clients attached to the current buffer
+local function lsp_clients()
+  local buf = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_clients({ bufnr = buf })
+  if #clients == 0 then
+    return 'No LSP'
+  end
+  local names = {}
+  for _, client in ipairs(clients) do
+    table.insert(names, client.name)
+  end
+  return ' ' .. table.concat(names, ', ')
+end
+
 -- configure lualine with modified theme
 lualine.setup {
   options = {
@@ -19,11 +33,11 @@ lualine.setup {
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { right = '' }, right_padding = 2 }, 
+      { 'mode', separator = { right = '' }, right_padding = 2 },
     },
     lualine_b = { 'filename', 'branch' },
-    lualine_c = {},
-    lualine_x = {},
+    lualine_c = { 'diagnostics' },
+    lualine_x = { lsp_clients },
     lualine_y = { 'filetype', 'fileformat' },
     lualine_z = {
       { 'location', separator = { left = '' }, left_padding = 2 },
